d3568959e5934de79390a7dfd0778048fae849f9
Higor Gabriel Santana Magalhães <higorgb2001@gmail.com>
2023-10-22 19:12:44 -0300

    inserir comentário

--- buggy0.c
+++ buggy0.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
+//oi
 int main(void)
 {
     // Print 10 hashes
     for (int i = 0; i < 10; i++)
     {
-
+        printf("\n");
         printf("i is now %i\n", i); //value of i before print #
         printf("#\n");
     }
