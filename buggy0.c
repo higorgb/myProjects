4e70d95108ebb7e63f8eda165c37ab04d424553a
higorgb <73893249+higorgb@users.noreply.github.com>
2023-10-22 19:11:51 -0300

    Add files via upload

---
+++ buggy0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Print 10 hashes
+    for (int i = 0; i < 10; i++)
+    {
+
+        printf("i is now %i\n", i); //value of i before print #
+        printf("#\n");
+    }
+}
